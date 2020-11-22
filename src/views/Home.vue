0074668210bd4329e54b07c665a8abb26729dae0
makki-villaluz <john_matthew88@outlook.ph>
2020-11-22 20:50:38 +0800

    added list of features to the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-jumbotron>
+      <b-container>
+        <h1 class="display-3">Team 2 Project</h1>
+        <p>Present 5 features we've implemented for CS 198</p>
+      </b-container>
+    </b-jumbotron>
+
+    <div style="text-align: left">
+      <b-container>
+        <h1 class="display-4">Features</h1>
+      </b-container>
+      <b-container style="margin-top: 20px">
+        <h2>Total Distance Travelled</h2>
+        <p>Uses the Haversine formula to calculate the total distance travelled given a GPX file</p>
+      </b-container>
+      <b-container style="margin-top: 20px">
+        <h2>Speeding Violation Detector</h2>
+        <p>Determines if a speeding violation (in km/hr) occured for a given duration of time (in minutes). This can detect speeding in 2 ways: from the speed readings in the GPX file ('Explicit'), or by calcuating the speed from the latitude and longitude ('Location')</p>
+      </b-container>
+      <b-container style="margin-top: 20px">
+        <h2>Stop Violation Detector</h2>
+        <p>Determines if a stop violation occured. A vehicle is only allowed to stay in a given location for a defined duration of time The user must input the upper left and lower right points of the location, and the minimum and maximum allowed time (in seconds)</p>
+      </b-container>
+      <b-container style="margin-top: 20px">
+        <h2>Liveness Checking</h2>
+        <p>Determines the total time a vehicle is "alive" or active. A vehicle is considered alive if the gaps between GPS readings are less than a given time limit</p>
+      </b-container>
+      <b-container style="margin-top: 20px">
+        <h2>Loop Counter</h2>
+        <p>Calculates the number of loops a vehicle makes in a route, given a grid size in km</p>
+      </b-container>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style scoped>
+
+</style>
