4bd97179003cbe8cfbb4f0baa86e2ad222122c6e
makki-villaluz <john_matthew88@outlook.ph>
2020-11-23 00:51:40 +0800

    added table display for gps trajectories

--- src/views/Trajectory.vue
+++ src/views/Trajectory.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="trajectory">
-    <h1>This is the vehicle trajectory page</h1>
+    <div style="text-align: left, margin: 20px">
+      <h1>GPS Trajectories of Vehicles</h1>
+    </div>
+    <b-table fixed hover light :items="trajectories" :fields="fields"></b-table>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "Trajectory"
+  name: "Trajectory",
+  data() {
+    return {
+      fields: ['id', 'name', 'filename'],
+      trajectories: []
+    }
+  },
+  methods: {
+    uploadTrajectory() {
+      console.log('upload');
+    }
+  },
+  created() {
+    axios.get("http://localhost:5000/vehicle")
+      .then(res => this.trajectories = res.data)
+      .catch(err => console.log(err));
+  }
 }
 </script>
