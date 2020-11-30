b1b1b4771b41f713432d31047f4ef244f305060e
makki-villaluz <john_matthew88@outlook.ph>
2020-12-01 01:37:16 +0800

    added vue2-leaflet dependency

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import "./plugins/bootstrap-vue";
+
+import './plugins/bootstrap-vue';
+
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 Vue.config.productionTip = false
 
