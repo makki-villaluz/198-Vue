76ee454f171d2b52a9f3d3a013e2aaf2894f37db
makki-villaluz <john_matthew88@outlook.ph>
2020-12-01 13:31:24 +0800

    fixed formatting of page elements with respect to the map

--- src/views/Trajectory.vue
+++ src/views/Trajectory.vue
@@ -6,24 +6,41 @@
     <b-row>
       <b-col>
         <b-card>
-          <b-form @submit.prevent="uploadTrajectory">
+          <template #header>
             <h3>Upload Vehicle Trajectory GPX File</h3>
-            <b-form-group label="Name" label-cols="2">
-              <b-form-input class="w-100 form-control-sm" required v-model="form.name"></b-form-input>
-            </b-form-group>
-            <div style="margin-bottom:18px">    
-              <b-form-file class="w-25" v-model="form.gpx_file" required plain></b-form-file>
+          </template>
+          <b-form @submit.prevent="uploadTrajectory">
+            <div style="margin: 30px 0 30px">
+              <b-form-group label="Input Name of File:">
+                <b-form-input class="w-100 form-control-sm" required v-model="form.name"></b-form-input>
+              </b-form-group>
             </div>
-            <b-button type="submit" variant="outline-primary" style="float: right">Upload</b-button>
+            <b-form-file class="w-25" v-model="form.gpx_file" required plain></b-form-file>
+            <div style="margin-top: 40px">
+              <b-button type="submit" variant="outline-primary" style="float: right">Upload</b-button>
+            </div>
           </b-form>
         </b-card>
       </b-col>
       <b-col cols="8">
-        <h1 style="text-align: center">Map should appear here</h1>
+        <LMap>
+          <LTileLayer 
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          ></LTileLayer>
+        </LMap>
       </b-col>
     </b-row>
     <div style="margin-top: 20px">
-      <b-table outlined fixed hover light ref="table" sticky-header="500px" head-variant="light" :items="trajectories" :fields="table.fields">
+      <b-table outlined fixed hover selectable light 
+        ref="table" 
+        sticky-header="400px" 
+        head-variant="light" 
+        @row-selected="rowSelected" 
+        select-mode="single"
+        :items="trajectories" 
+        :fields="table.fields"
+      >
         <template #cell(actions)="row">
           <b-button variant="outline-info" to="/route">Analyze</b-button>
           <b-button variant="outline-primary" v-b-modal="'edit-modal'" @click="modalInfo(row.item.id, row.item.name)">Edit</b-button>
@@ -38,9 +55,11 @@
         {{ modal.title }}
       </template>
       <b-form @submit.prevent="editTrajectory">
-        <b-form-group label="New Name" label-cols="3">
-          <b-form-input required class="w-100 form-control-sm" v-model="modal.name"></b-form-input>
-        </b-form-group>
+        <div style="margin: 20px 0 40px">
+          <b-form-group label="Input New Name of File:">
+            <b-form-input required class="w-100 form-control-sm" v-model="modal.name"></b-form-input>
+          </b-form-group>
+        </div>
         <b-button type="submit" variant="outline-primary" style="float: right">Edit</b-button>
       </b-form>
     </b-modal>
@@ -48,7 +67,7 @@
     <b-modal hide-footer id="delete-modal" @hidden="resetModal">
       <template #modal-title>
         <p>Deleting GPX File:</p>
-        {{ modal.title}}
+        {{ modal.title }}
       </template>
       <p>Are you sure you want to delete this GPX File?</p>
       <b-form @submit.prevent="deleteTrajectory">
@@ -60,9 +79,14 @@
 
 <script>
 import axios from "axios";
+import { LMap, LTileLayer } from 'vue2-leaflet';
 
 export default {
   name: "Trajectory",
+  components: {
+    LMap,
+    LTileLayer,
+  },
   data() {
     return {
       form: {
@@ -77,17 +101,21 @@ export default {
         id: null,
         name: "",
       },
+      selected: [],
       trajectories: [],
     }
   },
   methods: {
-    modalInfo(id, name){
+    modalInfo(id, name) {
       this.modal.id = id;
       this.modal.title = name;
     },
-    resetModal(){
+    resetModal() {
       this.modal.name = "";
       this.modal.id = null;
+    },
+    rowSelected(selected) {
+      this.selected = selected;
     },
     uploadTrajectory() {
       const formData = new FormData();
@@ -102,7 +130,7 @@ export default {
       this.form.name = '';
       this.form.gpx_file = null;
     },
-    editTrajectory(){
+    editTrajectory() {
       const formData = new FormData();
 
       formData.append("name", this.modal.name)
@@ -118,7 +146,7 @@ export default {
       this.modal.id = null;
       this.$bvModal.hide("edit-modal");
     },
-    deleteTrajectory(){
+    deleteTrajectory() {
       axios.delete("http://localhost:5000/vehicle/" + this.modal.id.toString())
         .then(res => this.trajectories = this.trajectories.filter(trajectory => trajectory.id !== res.data.id))
         .catch(err => console.log(err));
