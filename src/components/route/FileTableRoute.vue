ddb92a19079a4ab335538d092a5df52bdef43d60
makki-villaluz <john_matthew88@outlook.ph>
2020-12-03 03:05:48 +0800

    added table

---
+++ src/components/route/FileTableRoute.vue
@@ -0,0 +1,93 @@
+<template>
+	<div>
+		<b-table outlined fixed hover selectable light
+			ref="table"
+			sticky-header="400px"
+			head-variant="light"
+			@row-selected="rowSelected"
+			select-mode="single"
+			:items="routes"
+			:fields="table.fields"
+		>
+			<template #cell(actions)="row">
+				<b-button style="margin: 0 5px 0" variant="outline-primary" v-b-modal="'edit-modal'" @click="modalInfo(row.item.id, row.item.name)">Edit</b-button>
+				<b-button style="margin: 0 5px 0" variant="outline-danger" v-b-modal="'delete-modal'" @click="modalInfo(row.item.id, row.item.name)">Delete</b-button>
+			</template>
+		</b-table>
+
+		<b-modal hide-footer id="edit-modal" @hidden="resetModal">
+			<template #modal-title>
+				<p>Editing GPX File:</p>
+				{{ modal.title }}
+			</template>
+			<b-form @submit.prevent="editRoute">
+				<div style="margin: 20px 0 40px">
+					<b-form-group label="Input New Name of File:">
+						<b-form-input required class="w-100 form-control-sm" v-model="modal.name"></b-form-input>
+					</b-form-group>
+					<b-form-group label="Input New Cell Size of File:">
+						<b-form-input required class="w-100 form-control-sm" v-model="modal.cell_size"></b-form-input>
+					</b-form-group>
+				</div>
+				<b-button type="submit" variant="outline-primary" style="float: right">Edit</b-button>
+			</b-form>
+		</b-modal>
+		
+		<b-modal hide-footer id="delete-modal" @hidden="resetModal">
+			<template #modal-title>
+				<p>Deleting GPX File:</p>
+				{{ modal.title }}
+			</template>
+			<p style="margin: 20px 0 40px">Are you sure you want to delete this GPX File?</p>
+			<b-form @submit.prevent="deleteRoute">
+				<b-button type="submit" variant="outline-danger" style="float: right">Delete</b-button>
+			</b-form>
+		</b-modal>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "FileTableRoute",
+	props: ["routes"],
+	data() {
+		return {
+			table: {
+				selected: [],
+				fields: ["id", "name", {key: "cell_size", label: "Cell Size (km)"}, "filename", "actions"],
+			},
+			modal: {
+				title: "",
+				id: null,
+				name: "",
+				cell_size: null,
+			}
+		}
+	},
+	methods: {
+		modalInfo(id, name) {
+			this.modal.id = id;
+			this.modal.title = name;
+		},
+		resetModal() {
+			this.modal.id = null;
+			this.modal.cell_size = null;
+			this.modal.name = "";
+			this.modal.title = "";
+		},
+		rowSelected(selected) {
+			this.$emit("row-selected", selected);
+		},
+		editRoute() {
+			this.$emit("edit-route", this.modal.id, this.modal.name, this.modal.cell_size)
+			this.resetModal();
+			this.$bvModal.hide("edit-modal");
+		},
+		deleteRoute() {
+			this.$emit("delete-route", this.modal.id);
+			this.resetModal();
+			this.$bvModal.hide("delete-modal");
+		}
+	},
+}
+</script>
--- src/views/Route.vue
+++ src/views/Route.vue
@@ -1,11 +1,75 @@
 <template>
-  <div class="route">
-    <h1>This is the vehicle route page</h1>
+  <div class="route container-xl" style="padding-top: 30px">
+    <b-row>
+      <b-col>
+        Upload here
+      </b-col>
+      <b-col cols="8">
+        Map here
+      </b-col>
+    </b-row>
+    <FileTableRoute 
+      :routes="routes"
+      v-on:row-selected="rowSelected"
+      v-on:edit-route="editRoute"
+      v-on:delete-route="deleteRoute"
+    />
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import FileTableRoute from "@/components/route/FileTableRoute";
+
 export default {
-  name: "Route"
+  name: "Route",
+  components: {
+    FileTableRoute,
+  },
+  data() {
+    return {
+      routes: [],
+    }
+  },
+  methods: {
+    rowSelected(selected) {
+      console.log(selected)
+    },
+    uploadTrajectory(name, cell_size, gpx_file) {
+      const formData = new FormData();
+
+      formData.append("name", name);
+      formData.append("cell_size", cell_size);
+      formData.append("gpx_file", gpx_file);
+
+      axios.post("http://localhost:5000/vehicle", formData)
+        .then(res => this.routes = [...this.routes, res.data])
+        .catch(err => console.log(err));
+    },
+    editRoute(id, name, cell_size) {
+      const formData = new FormData();
+
+      formData.append("name", name);
+      formData.append("cell_size", cell_size);
+
+      axios.put("http://localhost:5000/route/" + id.toString(), formData)
+        .then(res => {
+          const index = this.routes.findIndex(route => route.id == res.data.id);
+          this.routes[index].name = res.data.name;
+          this.routes[index].cell_size = res.data.cell_size;
+        })
+        .catch(err => console.log(err));
+    },
+    deleteRoute(id) {
+      axios.delete("http://localhost:5000/route/" + id.toString())
+        .then(res => this.routes = this.routes.filter(route => route.id !== res.data.id))
+        .catch(err => console.log(err));
+    },
+  },
+  created() {
+    axios.get("http://localhost:5000/route")
+      .then(res => this.routes = res.data)
+      .catch(err => console.log(err));
+  }
 }
 </script>
