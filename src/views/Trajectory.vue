3395f3358a19562d41f550df8996534c9a356074
makki-villaluz <john_matthew88@outlook.ph>
2020-12-03 01:04:50 +0800

    added map to show selected gpx file from the table

--- src/views/Trajectory.vue
+++ src/views/Trajectory.vue
@@ -7,11 +7,9 @@
         />
       </b-col>
       <b-col cols="8">
-        <LMap>
-          <LTileLayer 
-            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          ></LTileLayer>
+        <LMap :zoom="map.zoom" :center="map.center" >
+          <LTileLayer :url="map.url" :attribution="map.attribution"></LTileLayer>
+          <LGeoJson :geojson="map.geojson" :options="map.options"></LGeoJson>
         </LMap>
       </b-col>
     </b-row>
@@ -29,7 +27,8 @@
 
 <script>
 import axios from "axios";
-import { LMap, LTileLayer } from "vue2-leaflet";
+import L from "leaflet";
+import { LMap, LTileLayer, LGeoJson} from "vue2-leaflet";
 import FileTable from "@/components/trajectory/FileTable";
 import UploadCard from "@/components/trajectory/UploadCard";
 
@@ -38,6 +37,7 @@ export default {
   components: {
     LMap,
     LTileLayer,
+    LGeoJson,
     FileTable,
     UploadCard,
   },
@@ -45,14 +45,46 @@ export default {
     return {
       table: {
         fields: ["id", "name", "filename", "actions"],
-        selected: []
+      },
+      map: {
+        zoom: 0,
+        center: [0, 0],
+        url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        options: {
+          style: function () {
+            return {
+              color: "blue"
+            }
+          },
+          pointToLayer: function (feature, latlng) {
+            return new L.CircleMarker(latlng, {
+              radius: 1,
+              fillOpacity: 0.85
+            })
+          }
+        },
+        geojson: null,
       },
       trajectories: [],
     }
   },
   methods: {
     rowSelected(selected) {
-      this.table.selected = selected;
+      if (selected.length) {
+        axios.get("http://localhost:5000/vehicle/" + selected[0].id.toString())
+          .then(res => {
+            this.map.center = [res.data.geojson.coordinates[0][1], res.data.geojson.coordinates[0][0]];
+            this.map.geojson = res.data.geojson;
+          })
+          .catch(err => console.log(err));
+        this.map.zoom = 12;
+      }
+      else {
+        this.map.zoom = 0;
+        this.map.center = [0, 0];
+        this.map.geojson = null;
+      }
     },
     uploadTrajectory(name, gpx_file) {
       const formData = new FormData();
