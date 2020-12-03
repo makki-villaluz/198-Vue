5c4bb7775b65650a9b5ec7db7c2a964443438ae3
makki-villaluz <john_matthew88@outlook.ph>
2020-12-03 12:36:52 +0800

    added UploadCardRouter and Map

---
+++ src/components/route/UploadCardRoute.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<b-card>
+			<template #header>
+				<h3>Upload Route Trajectory GPX File</h3>
+			</template>
+			<b-form @submit.prevent="uploadRoute">
+				<div style="margin: 30px 0 30px">
+					<b-form-group label="Input Name of File:">
+						<b-form-input class="w-100 form-control-sm" required v-model="name"></b-form-input>
+					</b-form-group>
+					<b-form-group label="Input Cell Size">
+						<b-form-input class="w-100 form-control-sm" required v-model="cell_size"></b-form-input>
+					</b-form-group>
+				</div>
+				<b-form-file class="w-25" v-model="gpx_file" required plain></b-form-file>
+				<div style="margin-top: 40px">
+					<b-button type="submit" variant="outline-primary" style="float: right">Upload</b-button>
+				</div>
+			</b-form>
+		</b-card>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "UploadCardRoute",
+	data() {
+		return {
+			name: "",
+			cell_size: null,
+			gpx_file: null,
+		}
+	},
+	methods: {
+		uploadRoute() {
+			this.$emit("upload-route", this.name, this.cell_size, this.gpx_file);
+			this.name = "";
+			this.cell_size = null;
+			this.gpx_file = null;
+		}
+	}
+}
+</script>
--- src/views/Route.vue
+++ src/views/Route.vue
@@ -2,50 +2,98 @@
   <div class="route container-xl" style="padding-top: 30px">
     <b-row>
       <b-col>
-        Upload here
+        <UploadCardRoute v-on:upload-route="uploadRoute"/>
       </b-col>
       <b-col cols="8">
-        Map here
+        <LMap :zoom="map.zoom" :center="map.center">
+          <LTileLayer :url="map.url" :attribution="map.attribution"></LTileLayer>
+          <LGeoJson :geojson="map.geojson" :options="map.options"></LGeoJson>
+        </LMap>
       </b-col>
     </b-row>
-    <FileTableRoute 
-      :routes="routes"
-      v-on:row-selected="rowSelected"
-      v-on:edit-route="editRoute"
-      v-on:delete-route="deleteRoute"
-    />
+    <div style="margin-top: 30px">
+      <FileTableRoute 
+        :routes="routes"
+        v-on:row-selected="rowSelected"
+        v-on:edit-route="editRoute"
+        v-on:delete-route="deleteRoute"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import L from "leaflet";
+import { LMap, LTileLayer, LGeoJson } from "vue2-leaflet";
 import FileTableRoute from "@/components/route/FileTableRoute";
+import UploadCardRoute from "@/components/route/UploadCardRoute";
 
 export default {
   name: "Route",
   components: {
+    LMap,
+    LTileLayer,
+    LGeoJson,
     FileTableRoute,
+    UploadCardRoute,
   },
   data() {
     return {
+      map: {
+        zoom: 0,
+        center: [0, 0],
+        url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        options: {
+          style: function () {
+            return {
+              color: "blue"
+            }
+          },
+          pointToLayer: function (feature, latlng) {
+            return new L.CircleMarker(latlng, {
+              radius: 1,
+              fillOpacity: 0.85
+            })
+          }
+        },
+        geojson: null,
+      },
       routes: [],
     }
   },
   methods: {
     rowSelected(selected) {
-      console.log(selected)
+      if (selected.length) {
+        axios.get("http://localhost:5000/route/" + selected[0].id.toString())
+          .then(res => {
+            this.map.center = [res.data.geojson.coordinates[0][1], res.data.geojson.coordinates[0][0]];
+            this.map.geojson = res.data.geojson;
+          })
+          .catch(err => console.log(err));
+        this.map.zoom = 12;
+      }
+      else {
+        this.map.zoom = 0;
+        this.map.center = [0, 0];
+        this.map.geojson = null;
+      }
     },
-    uploadTrajectory(name, cell_size, gpx_file) {
-      const formData = new FormData();
-
-      formData.append("name", name);
-      formData.append("cell_size", cell_size);
-      formData.append("gpx_file", gpx_file);
-
-      axios.post("http://localhost:5000/vehicle", formData)
-        .then(res => this.routes = [...this.routes, res.data])
-        .catch(err => console.log(err));
+    uploadRoute(name, cell_size){
+      console.log(name, cell_size);
     },
+    // uploadRoute(name, cell_size, gpx_file) {
+    //   const formData = new FormData();
+
+    //   formData.append("name", name);
+    //   formData.append("cell_size", cell_size);
+    //   formData.append("gpx_file", gpx_file);
+
+    //   axios.post("http://localhost:5000/vehicle", formData)
+    //     .then(res => this.routes = [...this.routes, res.data])
+    //     .catch(err => console.log(err));
+    // },
     editRoute(id, name, cell_size) {
       const formData = new FormData();
 
