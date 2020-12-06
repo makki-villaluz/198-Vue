6b47abb3da55375aeffe773fa8a992f5162cd41b
makki-villaluz <john_matthew88@outlook.ph>
2020-12-06 19:53:32 +0800

    updated names of links

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -2,9 +2,9 @@
   <b-navbar type="dark" variant="dark" fixed-top>
     <b-navbar-brand to="/">NDSG</b-navbar-brand>
     <b-navbar-nav>
-      <b-nav-item to="/trajectory">Trajectory</b-nav-item>
-      <b-nav-item to="/route">Route</b-nav-item>
-      <b-nav-item to="/stop">Stop</b-nav-item>
+      <b-nav-item to="/trajectories">Trajectories</b-nav-item>
+      <b-nav-item to="/routes">Routes</b-nav-item>
+      <b-nav-item to="/stops">Stops</b-nav-item>
     </b-navbar-nav>
   </b-navbar>
 </template>
