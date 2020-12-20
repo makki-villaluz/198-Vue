89a7eda44bd696e781bdb9db0b9ce3cfbac9c8be
makki-villaluz <john_matthew88@outlook.ph>
2020-12-20 23:47:26 +0800

    fixed card height, adding of stops, validation on stop coordinates

--- src/components/stops/UploadStops.vue
+++ src/components/stops/UploadStops.vue
@@ -5,49 +5,109 @@
 				<h3>Upload Stop Coordinates</h3>
 			</template>
 			<div v-if="addStopsPage === 0">
-				<b-form @submit.prevent="goToAddStopsPage">
-					<b-form-group label="Input Name:">
-						<b-form-input required class="w-100 form-control-sm" v-model="name"></b-form-input>
-					</b-form-group>
-					<b-form-group label="Input Minimum Time:">
-						<b-form-input required class="w-100 form-control-sm" v-model="min_time"></b-form-input>
-					</b-form-group>
-					<b-form-group label="Input Maximum Time:">
-						<b-form-input required class="w-100 form-control-sm" v-model="max_time"></b-form-input>
-					</b-form-group>
-					<b-button
-						type="submit"
-						style="float: right"
-						variant="outline-info"
-					>Add Points</b-button>
+				<b-form @submit.prevent="goToNextPage">
+					<div style="margin-top: 6px">
+						<b-form-group label="Input Name:">
+							<b-form-input required class="w-100 form-control-sm" v-model="name"></b-form-input>
+						</b-form-group>
+						<b-form-group label="Minimum Time (seconds):" label-cols="8">
+							<b-form-input required class="form-control-sm" v-model="min_time"></b-form-input>
+						</b-form-group>
+						<b-form-group label="Maximum Time (seconds):" label-cols="8">
+							<b-form-input required class="form-control-sm" v-model="max_time"></b-form-input>
+						</b-form-group>
+					</div>
+					<div style="margin-top: 5px">
+						<b-button
+							type="submit"
+							style="float: right"
+							variant="outline-info"
+						>Add Points</b-button>
+					</div>
 				</b-form>
 			</div>
-			<div v-else>
+			<div v-else-if="addStopsPage === 1">
+					<b-form @submit.prevent="goToNextPage">
+						<div style="margin: 30px 0 30px">
+							<p>Input Top Left Point</p>
+							<b-form-group label="Latitude Point 1:" label-cols="6">
+								<b-form-input required class="form-control-sm" v-model="inputStop.lat1"></b-form-input>
+							</b-form-group>
+							<b-form-group label="Longitude Point 1:" label-cols="6">
+								<b-form-input required class="form-control-sm" v-model="inputStop.long1"></b-form-input>
+							</b-form-group>
+						</div>
+						<b-button
+							type="submit"
+							variant="outline-info"
+							style="float: right"
+						>Add Point</b-button>
+					</b-form>
+			</div>
+			<div v-else-if="addStopsPage === 2">
 				<b-form @submit.prevent="addPoints">
-					<p>Input Top Left and Bottom Right Points</p>
-					<b-form-group label="Latitude Pt.1:" label-cols="5">
-						<b-form-input required class="form-control-sm" v-model="inputStop.lat1"></b-form-input>
-					</b-form-group>
-					<b-form-group label="Longitude Pt.1:" label-cols="5">
-						<b-form-input required class="form-control-sm" v-model="inputStop.long1"></b-form-input>
-					</b-form-group>
-					<b-form-group label="Latitude Pt.2:" label-cols="5">
-						<b-form-input required class="form-control-sm" v-model="inputStop.lat2"></b-form-input>
-					</b-form-group>
-					<b-form-group label="Longitude Pt.2:" label-cols="5">
-						<b-form-input required class="form-control-sm" v-model="inputStop.long2"></b-form-input>
-					</b-form-group>
+					<div style="margin: 30px 0 30px">
+						<p>Input Bottom Right Point</p>
+						<b-form-group label="Latitude Point 2:" label-cols="6">
+							<b-form-input required class="form-control-sm" v-model="inputStop.lat2"></b-form-input>
+						</b-form-group>
+						<b-form-group label="Longitude Point 2:" label-cols="6">
+							<b-form-input required class="form-control-sm" v-model="inputStop.long2"></b-form-input>
+						</b-form-group>
+					</div>
+					<b-button
+						type="submit"
+						variant="outline-info"
+						style="float: right"
+					>Add Point</b-button>
+				</b-form>
+			</div>
+			<div v-else-if="addStopsPage === 3">
+				<div style="height: 192px">
+					<b-alert 
+						:show="3"
+						:variant="alert.variant"
+						@dismissed="alert.showing = false"
+						style="padding: 5px 20px 5px"
+					>
+						{{ alert.message }}
+					</b-alert>
+					<div v-if="alert.showing == false" style="padding-top: 25px">
+					</div>
+					<b-row>
+						<b-col cols="5" style="padding-right: 0">
+							<h5>Name:</h5>
+							<h5>Min Time:</h5>
+							<h5>Max Time:</h5>
+							<h5>Stops:</h5>
+						</b-col>
+						<b-col cols="7" style="padding-left: 0">
+							<h5>{{ name }}</h5>
+							<h5>{{ min_time }}s</h5>
+							<h5>{{ max_time }}s</h5>
+							<h5>{{ stops.length }}</h5>
+						</b-col>
+					</b-row>
+				</div>
+				<div v-if="stops.length">
 					<b-button
 						style="float: right"
 						variant="outline-primary"
 						@click="uploadStops"
-					>Submit</b-button>
+					>Upload</b-button>
+				</div>
+				<div v-else>
 					<b-button
-						type="submit"
-						style="margin-right: 5px; float: right"
-						variant="outline-info"
-					>Add Points</b-button>
-				</b-form>
+						style="float: right"
+						variant="outline-primary"
+						disabled
+					>Upload</b-button>
+				</div>
+				<b-button
+					variant="outline-info"
+					style="margin-right: 5px; float: right"
+					@click="goToAddStopsPage"
+				>Add More Points</b-button>
 			</div>
 		</b-card>
 	</div>
@@ -70,14 +130,27 @@ export default {
 				lat2: null,
 				long2: null,
 			},
+			alert: {
+				message: "",
+				variant: "",
+				showing: true,
+			}
 		}
 	},
 	methods: {
-		goToAddStopsPage() {
-			this.addStopsPage = 1;
+		goToNextPage() {
+			this.addStopsPage += 1;
 		},
 		goToInfoStopsPage() {
 			this.addStopsPage = 0;
+			this.clearInfoStopsPage();
+			this.clearAddStopsPages();
+			this.clearAlert();
+		},
+		goToAddStopsPage() {
+			this.addStopsPage = 1;
+			this.clearAddStopsPages();
+			this.clearAlert();
 		},
 		clearInfoStopsPage() {
 			this.name = "";
@@ -86,25 +159,52 @@ export default {
 			this.max_time = null;
 			this.stops = [];
 		},
-		clearAddStopsPage() {
+		clearAddStopsPages() {
 			this.inputStop.lat1 = null;
 			this.inputStop.long1 = null;
 			this.inputStop.lat2 = null;
 			this.inputStop.long2 = null;
 		},
+		clearAlert() {
+			this.alert.variant = "";
+			this.alert.message = "";
+			this.alert.showing = true;
+		},
+		validateStopPoints() {
+			if (Number.isNaN(Number(this.inputStop.lat1)) || Number.isNaN(Number(this.inputStop.long1)) || Number.isNaN(Number(this.inputStop.lat2)) || Number.isNaN(Number(this.inputStop.long2))) {
+				this.alert.variant = "danger";
+				this.alert.message = "Invalid Numbers"
+
+				return false;
+			}
+			else if (Number(this.inputStop.lat1) >= Number(this.inputStop.lat2) || Number(this.inputStop.long1) <= Number(this.inputStop.long2)) {
+				this.alert.variant = "danger";
+				this.alert.message = "Invalid Top Left, Bottom Right Points"
+			
+				return false;
+			}
+			else {
+				this.alert.variant = "success";
+				this.alert.message = "Stop Added"
+			
+				return true;
+			}
+		},
 		createFilename() {
 			return this.name.split(" ").join("_").toLowerCase() + ".gpx";
 		},
 		addPoints() {
-			const stop = {
-				"lat1": this.inputStop.lat1,
-				"long1": this.inputStop.long1,
-				"lat2": this.inputStop.lat2,
-				"long2": this.inputStop.long2,
-			};
+			if (this.validateStopPoints()){
+				const stop = {
+					"lat1": this.inputStop.lat1,
+					"long1": this.inputStop.long1,
+					"lat2": this.inputStop.lat2,
+					"long2": this.inputStop.long2,
+				};
 
-			this.stops.push(stop);
-			this.clearAddStopsPage();
+				this.stops.push(stop);
+			}
+			this.goToNextPage();
 		},
 		uploadStops() {
 			this.filename = this.createFilename();
@@ -112,8 +212,6 @@ export default {
 			this.$emit("upload-stop", this.name, this.filename, this.min_time, this.max_time, this.stops)
 			
 			this.goToInfoStopsPage();
-			this.clearInfoStopsPage();
-			this.clearAddStopsPage();
 		},
 	},
 }
