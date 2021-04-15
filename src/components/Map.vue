8e9a28849d0bf2cba9e693f4cf472cfecd17b406
makki-villaluz <john_matthew88@outlook.ph>
2021-04-15 17:40:49 +0800

    added red and orange markers for the speeding and stop violations to show on the map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,22 +2,45 @@
 	<LMap :bounds="bounds" :center="center" :zoom="zoom">
 		<LTileLayer :url="url" :attribution="attribution"></LTileLayer>
 		<LGeoJson v-if="geojson !== null" :geojson="geojson" :options="options"></LGeoJson>
-		<LPolygon v-if="polygon !== null" :lat-lngs="polygon" color="red" weight="2" fillColor="red"></LPolygon>
+		<LPolygon v-if="polygon !== null" :lat-lngs="polygon" color="red" :weight="2" fillColor="red"></LPolygon>
+		<div v-if="speeding !== null">
+			<LMarker v-for="(item, index) in speeding" :lat-lng="[item.lat1, item.long1]" :key="index">
+				<LTooltip>Speeding Violation #{{ index + 1 }}</LTooltip>
+				<LIcon
+					icon-url="/static/redMarker.png"
+					:icon-size="[25, 41]"
+					:icon-anchor="[12, 41]"
+				></LIcon>
+			</LMarker>
+		</div>
+		<div v-if="stop !== null">
+			<LMarker v-for="(item, index) in stop_violations" :lat-lng="[item.x, item.y]" :key="index">
+				<LTooltip>Stop Violation #{{ item.violations }}</LTooltip>
+				<LIcon
+					icon-url="/static/orangeMarker.png"
+					:icon-size="[25, 41]"
+					:icon-anchor="[12, 41]"
+				></LIcon>
+			</LMarker>
+		</div>
 	</LMap>
 </template>
 
 <script>
 import L from "leaflet";
-import { LMap, LTileLayer, LGeoJson, LPolygon } from "vue2-leaflet";
+import { LMap, LTileLayer, LGeoJson, LPolygon, LMarker, LTooltip, LIcon } from "vue2-leaflet";
 
 export default {
 	name: "Map",
-	props: ["geojson", "polygon"],
+	props: ["geojson", "polygon", "speeding", "stop"],
   components: {
 		LMap,
 		LTileLayer,
 		LGeoJson,
 		LPolygon,
+		LMarker,
+		LTooltip,
+		LIcon
 	},
 	data() {
 		return {
@@ -38,7 +61,8 @@ export default {
 			},
 			bounds: null,
 			center: [0,0],
-			zoom: 0
+			zoom: 0,
+			stop_violations: [],
 		}
 	},
 	methods: {
@@ -77,6 +101,38 @@ export default {
 				this.center = [0,0];
 				this.zoom = 0;
 			}
+		},
+		"stop" (stop) {
+			const unique_stops = [];
+
+			const temp = {
+				x: stop[0].center_lat, 
+				y: stop[0].center_long,
+				violations: '1'
+			};
+			unique_stops.push(temp);
+
+			const length = stop.length;
+			for (var i = 1; i < length; i++) {
+				var match = false;
+				for (var j = 0; j < unique_stops.length; j++) {
+					console.log(stop[i].x)
+					if (stop[i].center_lat === unique_stops[j].x && stop[i].center_long === unique_stops[j].y) {
+						unique_stops[j].violations = unique_stops[j].violations.concat(", ".concat(i+1));
+						match = true;
+					}
+				}
+				if (match === false) {
+					const temp = {
+						x: stop[i].center_lat, 
+						y: stop[i].center_long,
+						violations: (i+1).toString()
+					};
+					unique_stops.push(temp);
+				}
+			}
+
+			this.stop_violations = unique_stops;
 		}
 	},
 }
