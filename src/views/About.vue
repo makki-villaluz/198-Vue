e73e239a1ed6ea7803073e107ff284c72f6b23e7
makki-villaluz <john_matthew88@outlook.ph>
2021-05-31 14:36:48 +0800

    added proper text for the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,17 +3,17 @@
     <div class="content">
       <b-card>
         <template #header>
-          <h2>About the Project</h2>
+          <h2>The Project</h2>
         </template>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dignissimos ullam corrupti natus doloremque exercitationem. Expedita tempora ratione ab consequuntur nihil voluptatem recusandae molestiae laborum quibusdam, dicta fuga enim dolorum?
+        This is the official website of JeePS, a part of the Arangkadata project by the University of the Philippines – Diliman, funded by the Department of Science and Technology, and supported by the Department of Transportation.
       </b-card>
     </div>
     <div class="content">
       <b-card>
         <template #header>
-          <h2>About the System</h2>
+          <h2>The System</h2>
         </template>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, maiores tempore ratione illum eligendi ea id dolorem aperiam animi consectetur ducimus accusantium sunt repellendus delectus, repudiandae fugit impedit velit voluptate.
+        JeePS is a system that enables you to monitor several service contracting measures, just by uploading a GPX file. These measures include counting the total distance travelled, checking for speeding violations, checking for stop violations, and counting the number of loops made. All the information is presented alongside a map for visualization. These features are especially important with the shift towards a service contracting scheme.
       </b-card>
     </div>
   </b-container>
