66a7ff76aca232e6afba87d86dbab7654d395062
makki-villaluz <john_matthew88@outlook.ph>
2021-06-09 17:21:01 +0800

    added fetching routes and stops for sending information to upload card and table components, updated upload and edit functions to include route_id and stops_id

--- src/views/Trajectories.vue
+++ src/views/Trajectories.vue
@@ -10,6 +10,8 @@
       <b-col>
         <UploadCardTrajectories
           :trajectories="trajectories"
+          :route_options="route_options"
+          :stops_options="stops_options"
           v-on:upload-trajectory="uploadTrajectory"
         />
       </b-col>
@@ -22,6 +24,8 @@
     <div style="margin-top: 30px">
       <FileTableTrajectories
         :trajectories="trajectories"
+        :route_options="route_options"
+        :stops_options="stops_options"
         v-on:row-selected="rowSelected"
         v-on:edit-trajectory="editTrajectory"
         v-on:delete-trajectory="deleteTrajectory"
@@ -31,7 +35,7 @@
 </template>
 
 <script>
-import { fetchTrajectory, postTrajectory, updateTrajectory, removeTrajectory, fetchTrajectories } from "@/api/index.js";
+import { fetchTrajectory, postTrajectory, updateTrajectory, removeTrajectory, fetchTrajectories, fetchRoutes, fetchStops } from "@/api/index.js";
 import FileTableTrajectories from "@/components/trajectories/FileTableTrajectories";
 import UploadCardTrajectories from "@/components/trajectories/UploadCardTrajectories";
 import Map from "@/components/Map";
@@ -54,6 +58,8 @@ export default {
         duration: 0,
       },
       trajectories: [],
+      route_options: [{value: null, text: "Please select an option"}],
+      stops_options: [{value: null, text: "Please select an option"}],
     }
   },
   methods: {
@@ -69,11 +75,13 @@ export default {
         this.map.geojson = null;
       }
     },
-    uploadTrajectory(name, gpx_file) {
+    uploadTrajectory(name, gpx_file, route_id, stops_id) {
       const formData = new FormData();
 
       formData.append("name", name);
       formData.append("gpx_file", gpx_file);
+      formData.append("route_id", route_id);
+      formData.append("stops_id", stops_id);
 
       postTrajectory(formData)
         .then(res => {
@@ -88,15 +96,21 @@ export default {
           this.alert.duration = 3;
         });
     },
-    editTrajectory(id, name) {
+    editTrajectory(id, name, route_id, stops_id) {
       const formData = new FormData();
 
-      formData.append("name", name)
+      formData.append("name", name);
+      formData.append("route_id", route_id);
+      formData.append("stops_id", stops_id);
 
       updateTrajectory(id, formData)
         .then(res => {
           const index = this.trajectories.findIndex(trajectory => trajectory.id == res.data.id);
           this.trajectories[index].name = res.data.name;
+          this.trajectories[index].route_id = res.data.route_id;
+          this.trajectories[index].route_name = res.data.route_name;
+          this.trajectories[index].stops_id = res.data.stops_id;
+          this.trajectories[index].stops_name = res.data.stops_name;
           this.alert.message = "File successfully edited";
           this.alert.variant = "success";
           this.alert.duration = 3;
@@ -131,6 +145,20 @@ export default {
     fetchTrajectories()
       .then(res => this.trajectories = res.data)
       .catch(err => console.log(err));
+    fetchRoutes()
+      .then(res => {
+        res.data.forEach(route => {
+          this.route_options.push({value: route.id, text: route.name});
+        });
+      })
+			.catch(err => console.log(err));
+    fetchStops()
+      .then(res => {
+        res.data.forEach(stop => {
+          this.stops_options.push({value: stop.id, text: stop.name});
+        });
+      })
+      .catch();
   }
 }
 </script>
